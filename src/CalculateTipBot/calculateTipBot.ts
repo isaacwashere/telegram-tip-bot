7c67d88f4784af05102bf4b8ba2529f1145e3670
Isaac Curtiss <ijcurtiss7@gmail.com>
2022-07-31 11:29:00 -0400

    add prod bot

--- src/CalculateTipBot/calculateTipBot.ts
+++ src/CalculateTipBot/calculateTipBot.ts
@@ -5,22 +5,16 @@ import {
   RATES,
 } from "../shared/constants";
 import { calculateTipAmount } from "../shared/utils";
-import { TEST_START_MESSAGE } from "./constants";
+import { CALCULATE_TIP_START_MESSAGE } from "./constants";
 
-/**
- * @description This is simply to be used as a playground for testing locally
- * @param bot should be passed to this
- */
-export const addTestBotCommands = (bot: Bot) => {
-  bot.start((ctx) => ctx.reply(TEST_START_MESSAGE));
+export const addCalculateTipBotCommands = (bot: Bot) => {
+  bot.start((ctx) => ctx.reply(CALCULATE_TIP_START_MESSAGE));
 
   bot.on("text", (ctx) => {
-    console.log("Local testing...", ctx);
     const totalCost = Number(ctx.update.message.text);
 
     if (!totalCost) {
-      console.log("Invalid Input", totalCost);
-      return bot.telegram.sendMessage(ctx.chat.id, ERROR_MESSAGE);
+      return bot.telegram.sendMessage(ctx.chat.id, ERROR_MESSAGE, {});
     }
 
     let ratesMessage = "";
@@ -34,6 +28,7 @@ export const addTestBotCommands = (bot: Bot) => {
 
       const tip = tipAmount.toFixed(FIXED_DECIMAL_AMOUNT);
       const total = totalAmount.toFixed(FIXED_DECIMAL_AMOUNT);
+
       const currentMessage = `${rate.readable} - ${tip} - ${total}`;
       ratesMessage = ratesMessage + `\n${currentMessage}`;
     });
